3e4e6c7e7565affb64acc99c029d755e5a4e005c
VICTORIA FIEDOTIN <86FI40903400@campus.economicas.uba.ar>
2022-03-30 22:37:35 -0300

    Agregar archivos de proyecto.

---
+++ Guia1_Ejercicio3/Program.cs
@@ -0,0 +1,11 @@
+﻿ConsoleKeyInfo tecla;
+
+do
+{
+    Console.WriteLine("Presione una tecla para visualizar el caracter y luego presione la tecla X para salir");
+
+    while (Console.KeyAvailable == false) ;
+
+    tecla = Console.ReadKey(true);
+    Console.WriteLine("Usted presionó la tecla '{0}'", tecla.Key);
+} while (tecla.Key != ConsoleKey.X);
